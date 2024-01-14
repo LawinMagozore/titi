a69638f8b52e4ed7fa9235cf89738fa360fe68fd
LawinMagozore <magozorel@gmail.com>
2024-01-14 02:02:41 +0200

    initial commit

---
+++ computerengineering.php
@@ -0,0 +1,106 @@
+<?php include('header.php'); ?>
+<!-- Content Wrapper. Contains page content -->
+<div class="mycontainer">
+
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1 style="color: #ffffff;" class="m-0">Computer Engineering </h1>
+                    </div>
+                    <!-- /.col -->
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="#">Register </a></li>
+                            <li class="breadcrumb-item active">Courses </li>
+                        </ol>
+                    </div>
+                    <!-- /.col -->
+
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- Info boxes -->
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title"> Duration 8 months </h3>
+                        <p> Registration Requirements: Computer studies or IT knowledge</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="banner-detail">
+                            <div class="banner-checkmark-list-items">
+                                <ul>
+                                    <li>Hands on projects and assignment</li>
+                                    <li>Full online learning</li>
+                                    <li>Computer Skills background</li>
+                                    <li>Software equipped</li>
+                                    <li>Certification</li>
+                                </ul>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+                <!-- /.row -->
+
+
+            </div>
+
+
+    </div>
+    <!-- Main content -->
+    <section class="content">
+        <div class="container-fluid">
+            <!-- Info boxes -->
+            <div class="card">
+                <div class="card-header">
+                    <h5 class="card-title"> More About Computer Engineering </h5>
+
+                </div>
+                <div class="card-body">
+
+
+                    <p>Computer engineering is a branch of engineering that integrates several fields of computer science and electronic engineering required to develop computer hardware and software. The Department of Software Engineering is the home of Computer Engineering programmes that focus on the development, prototyping and design of both software and hardware, as well as the integration of the two. </p>
+                    <h6>Programme Aim</h6>
+                    <p>provide a suitable, multi-disciplinary training for a career in the important and expanding field of Software Engineering.</p>
+                    <h6>Programme Objectives</h6>
+                    <p>The programme is designed such that on completion of its courses, the graduates should be able to:</p>
+
+                    <p> AThe program in computer engineering has a core-plus-elective structure.
+                        The core includes fundamental professional courses focused on digital hardware, digital systems design, software engineering, algorithms, electronics and communications, as well as developing a necessary foundation in mathematics,
+                        computer programming, electric circuits, the physical sciences and thermodynamics..</p>
+
+
+
+                    <h6>Career Prospects</h6>
+                    <p>The graduates of this programme are Software Engineers who provide a systematic approach to the analysis, design, assessment, implementation, test, maintenance and reengineering of software of different kinds.
+                        Computer Programmers specializing in the development of robust Software including :
+                        – Compiler Design;
+                        – Microprocessors and Embedded Systems;
+                        – Distributed Systems.
+                        Computer and information systems managers are responsible for the implementation and administration of technology within their organisations. They plan, coordinate, and direct research on the computer-related activities of firms. They oversee all technical aspects of an organization, such as software development, network security and internet operations.</p>
+
+                </div>
+            </div>
+            <!-- /.row -->
+
+            <button name="button" class="btn btn-primary form-control" onclick="document.location='registrationform.php'">Register</button>
+        </div>
+
+
+
+
+
+
+</div>
+<? include('footer.php') ?>
